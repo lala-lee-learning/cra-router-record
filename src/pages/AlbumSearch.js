3fdd946e1f68ee9e965298562e58bb4e94fd16c2
lala-lee-learning <lala-lee-learning@gmail.com>
2023-01-29 22:51:35 +0800

    React Router：搜尋及網址參數

--- src/pages/AlbumSearch.js
+++ src/pages/AlbumSearch.js
@@ -1,24 +1,36 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const api = "https://api.unsplash.com/search/photos";
 const accessId = process.env.REACT_APP_UNSPLASH_ACCESS;
 
 const AlbumSearch = () => {
+  const [search, setSearch] = useState('');
   const [list, setList] = useState([]);
-  const [search, setSearch] = useState('animal');
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // useEffect(() => {
+  //   // console.log(searchParams.get('query'));
+  //   setSearchParams({ query: 'building'});
+  // }, []);  
 
   useEffect(() => {
-    (async() => {
-      const response = await axios.get(
-        `${api}?client_id=${accessId}&query=${search}`
-      );
-      const {results} = response.data;
-      console.log(results);
-      setList(results);
-    })();
+    if(search) {
+      (async() => {
+        const response = await axios.get(
+          `${api}?client_id=${accessId}&query=${search}`
+        );
+        const {results} = response.data;
+        setList(results);
+      })();  
+    }
   }, [search]);
+
+  useEffect(() => {
+    setSearch(searchParams.get('query'));
+  }, [searchParams]);  
 
   return (<>
     這是搜尋頁面 {search}
@@ -27,7 +39,8 @@ const AlbumSearch = () => {
       defaultValue={search}
       onKeyUp={(e) => {
         if(e.code === 'Enter') {
-          setSearch(e.target.value);
+          // setSearch(e.target.value);
+          setSearchParams({query: e.target.value});
         }
       }}
     />
