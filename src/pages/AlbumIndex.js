9e6cb2b10d34ee78bc4184ad0fb87e97bee1d46c
lala-lee-learning <lala-lee-learning@gmail.com>
2023-01-29 17:11:39 +0800

    React Router：串接 Unsplash API

--- src/pages/AlbumIndex.js
+++ src/pages/AlbumIndex.js
@@ -1,6 +1,17 @@
+import { useOutletContext } from "react-router-dom";
+
 const AlbumIndex = () => {
+  const list = useOutletContext();
+  console.log('AlbumIndex:', list);
   return (
-    <div>這是相簿的首頁</div>
+    <div>
+      這是相簿的首頁
+      {list.map((item) => {
+      return (
+        <li key={item.id}>{item.id}</li>
+      );
+      })}
+    </div>
   );
 };
 
