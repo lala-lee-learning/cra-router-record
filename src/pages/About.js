acb414ff2abb9b43ee68bb748152b356ed5d1120
lala-lee-learning <lala-lee-learning@gmail.com>
2023-01-29 16:18:27 +0800

    React Router：環境準備

---
+++ src/pages/About.js
@@ -0,0 +1,7 @@
+const About = () => {
+  return (
+    <div>這是關於我的頁面</div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <div>這是首頁</div>
+  );
+};
+
+export default Home;
